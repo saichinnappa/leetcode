e72c3eecfa5503e7d44e267fe7d3af79cc792554
Sai Chinnappa <saisubramanyam.purruchinnappa@target.com>
2017-08-09 13:58:34 -0500

    pushing changes

--- src/com/leetcode/problems/TwoSums.java
+++ src/com/leetcode/problems/TwoSums.java
@@ -12,24 +12,33 @@ package com.leetcode.problems;
  return [0, 1].
  */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class TwoSums {
 
   public static void main(String args[]){
 
-    if(args.length != 2){
-      System.out.println("Invalid arguments! Must be exactly two arguments.");
+    int[] nums = {2, 7, 11, 15};
+    ArrayList<Integer> output = new ArrayList<Integer>();
+
+    int target = 9;
+    for(int i=0; i<nums.length; i++){
+      for(int j=1; j<nums.length; j++){
+        if(nums[i] + nums[j] == target){
+            output.add(i);
+            output.add(j);
+            break;
+        }
+      }
     }
-
-    if(args[0].length() < 2){
-      System.out.println("Input array must have atleast two elements");
+    if(output.size() != 0){
+      System.out.println(output);
     }
-    System.out.println(args[0].length());
-    int[] inputArray = new int[args[0].length()];
-
-//    for(String inputElement : args[0]){
-//    }
-
-
+    else{
+      System.out.println("No input found which sums up to target");
+    }
   }
 
 }
